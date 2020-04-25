4e54cd2be2043c9f792f2a4c174630d33851dd41
Omer <omkomilko3@gmail.com>
2020-04-25 13:36:16 +0200

    Updates

---
+++ precache-manifest.390dcca2df9511bd5cf773716db43413.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e742410bf396b869394b84a64e56127",
+    "url": "/rubiflix/index.html"
+  },
+  {
+    "revision": "33f47c7c1d7b47d99e49",
+    "url": "/rubiflix/static/css/main.0462fa5e.chunk.css"
+  },
+  {
+    "revision": "2cd1b9c759406df2a8d1",
+    "url": "/rubiflix/static/js/2.3b9f1ee1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rubiflix/static/js/2.3b9f1ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f47c7c1d7b47d99e49",
+    "url": "/rubiflix/static/js/main.a94ba7d3.chunk.js"
+  },
+  {
+    "revision": "07432ac1c3ff4134ccfd",
+    "url": "/rubiflix/static/js/runtime-main.b4babfa2.js"
+  },
+  {
+    "revision": "6580150fc1d30ff5a30daf15de5ab562",
+    "url": "/rubiflix/static/media/imdb.6580150f.png"
+  },
+  {
+    "revision": "9d3e28eac5f4db4fd1129da89aacdbf1",
+    "url": "/rubiflix/static/media/loading.9d3e28ea.svg"
+  },
+  {
+    "revision": "b05a40e9c76c44260bc2f8b3f0a13cce",
+    "url": "/rubiflix/static/media/logo.b05a40e9.png"
+  },
+  {
+    "revision": "6418f0050cf34ec33dc6c1c6e1e4c8cd",
+    "url": "/rubiflix/static/media/noimage.6418f005.png"
+  },
+  {
+    "revision": "09ed808c07a5abd25de5c9698abd89fe",
+    "url": "/rubiflix/static/media/star.09ed808c.png"
+  }
+]);
